155070b8fe1ef72a91ae538f69930f60b8c6f2db
Rechnoe <temirlantashen4@gmail.com>
2024-11-16 20:18:58 +0500

    test

---
+++ quiz_3/src/components/TodoItem.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+  interface TodoItem {
+    id: number, 
+    title: string,
+    priority: string, 
+    completed: boolean
+  }
+
+  const props = defineProps<{
+    item: TodoItem
+  }>()
+    const emit = defineEmits<{
+      (e: 'done', id: number): void
+      (e: 'delete', value: string): void
+    }>()
+  
+</script>
+
+<template>
+  <Transition>
+    <div class="item">
+      <input type="checkbox" @click="$emit('done', item.id)">
+      <div class="item__content" :class="{'completed': item.completed }">
+        {{ item.title }}
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 0.5s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+  .item {
+    border: solid;
+  }
+  .completed {
+    text-decoration: overline;
+  }
+</style>
---
+++ quiz_3/src/components/TodoList.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+  import { ref, computed } from 'vue';
+  import TodoItem from './TodoItem.vue';
+  let currId = 0
+  interface TodoItem {
+    id: number, 
+    title: string,
+    priority: string, 
+    completed: boolean
+  }
+  const todoList = ref<TodoItem[]>([ 
+  ])
+  const newItem = ref<TodoItem>(
+    {
+      id: currId++,
+      title: '',
+      priority: '', 
+      completed: false
+    }
+  )
+  const title = computed({
+    get() {
+      return newItem.value?.title || ''
+    },
+    set(title) {
+      if (typeof newItem.value?.title === 'string') {
+        newItem.value = { ...newItem.value, title }
+      }
+    },
+  })
+  const priority = computed({
+    get() {
+      return newItem.value?.priority || ''
+    },
+    set(priority) {
+      if (typeof newItem.value?.priority === 'string') {
+        newItem.value = { ...newItem.value, priority }
+      }
+    },
+  })
+  function addItem() {
+    todoList.value.push(newItem.value)
+  }
+</script>
+
+<template>
+  <input type="text" v-model="title"> <button @click="addItem()">Add task</button>
+  <select class="form-control" v-model="priority">
+    <option v-bind:value="'high'" >High</option>
+    <option v-bind:value="'medium'" >Medium</option>
+    <option v-bind:value="'medium'" >Low</option>
+  </select>
+  
+  <TransitionGroup name="list" tag="ul">
+    <li v-for="item in todoList" :key="item.id">
+      <TodoItem :item="item" @add="(i) => count += i"></TodoItem>
+    </li>
+  </TransitionGroup>
+</template>
+
+<style scoped>
+  input {
+    width: 5em;
+    height: 2em;
+  }
+  button {
+    height: 2em;
+  }
+  .list-enter-active,
+  .list-leave-active {
+    transition: all 0.5s ease;
+  }
+  .list-enter-from,
+  .list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+  }
+</style>
